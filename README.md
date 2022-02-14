fe2e7c89c5d1aec4ab1cdc566cfbd9c66e4450cc
Mykhailo Mozhaiskyi <mihail.mozhaiskii@gmail.com>
2022-02-14 14:37:08 +0100

    Init project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Wajve coding challenge
+
+## Getting Started
+To run project you need to have Flutter version 2.8.1.
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wajve',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Wajve'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+    );
+  }
+}
